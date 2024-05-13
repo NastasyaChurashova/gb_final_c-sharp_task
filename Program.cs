dd18ee76682eacac530392c8b1201bd5c31525e1
Nastasya Churashova <gregarious@list.ru>
2024-05-13 22:47:45 +0200

    method to calculate size  of new array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] CreateLessThanThreeCharStringArray(string[] charArray)
+{
+    int count = 0;
+    for (int i = 0; i < charArray.Length; i++)
+    {
+        if (charArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    int index = 0;
+
+    return result;
+}
